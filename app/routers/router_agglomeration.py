f5f504f9cd298f4808c2548e9aa04b9b809968fc
Leon <100690100+LeonDeTur@users.noreply.github.com>
2025-04-07 19:04:55 +0300

    feat(router_agglomeration): - added time ro agglomerations parameters

--- app/routers/router_agglomeration.py
+++ app/routers/router_agglomeration.py
@@ -45,31 +45,33 @@ async def get_href(
 
 @agglomeration_router.get("/build_agglomeration")
 async def get_agglomeration_endpoint(
-        region_id: int
+        region_id: int,
+        time: int=80
 ):
     try:
         region_model = await pop_frame_model_service.get_model(region_id)
         builder = AgglomerationBuilder(region=region_model)
-        agglomeration_gdf = builder.get_agglomerations()
+        agglomeration_gdf = builder.get_agglomerations(time=time)
+        agglomeration_gdf.to_crs(4326, inplace=True)
         result = json.loads(agglomeration_gdf.to_json())
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during agglomeration processing: {str(e)}")
 
 
-@agglomeration_router.get("/evaluate_city_agglomeration_statuss", response_model=Dict[str, Any])
-def evaluate_cities_in_agglomeration(
-        region_model: Region = Depends(
-            pop_frame_model_service.get_model
-        )
+@agglomeration_router.get("/evaluate_city_agglomeration_status", response_model=Dict[str, Any])
+async def evaluate_cities_in_agglomeration(
+        region_id: int,
+        time: int=80
 ):
     try:
+        region_model = await pop_frame_model_service.get_model(region_id)
         frame_method = PopulationFrame(region=region_model)
         gdf_frame = frame_method.build_circle_frame()
-
         builder = AgglomerationBuilder(region=region_model)
-        agglomeration_gdf = builder.get_agglomerations()
+        agglomeration_gdf = builder.get_agglomerations(time=time)
         towns_with_status = builder.evaluate_city_agglomeration_status(gdf_frame, agglomeration_gdf)
+        towns_with_status.to_crs(4326, inplace=True)
         result = json.loads(towns_with_status.to_json())
         return result
     except Exception as e:
